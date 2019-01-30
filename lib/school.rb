9358b2237b4a26b08c6b534addab65c005f4970f
Ethan Roberts <ejrobe@gmail.com>
2019-01-30 17:58:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+# roster empty hash
+# add_student, which adds to roster
+# grade (int)
+# sort alphabetically
+class School
+    attr_accessor :grade
+    attr_reader :name
+    
+    def initialize
+        @name
+        @grade
+    end
+    
+    def self.add_student(name, grade)
+        @name = name
+        @grade = grade
+    end
+
+    roster = {}
+    
+end
+
+binding.pry
